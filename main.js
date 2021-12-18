6f2d3815047872af2131e71bb754aa4e8ace371e
narahari-anvita <79611794+narahari-anvita@users.noreply.github.com>
2021-12-18 15:31:06 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log("Posenet is detected!");
+}
+
+function draw() {
+    background('#A8E6CE');
+    document.getElementById("square_side").innerHTML = "Width and Height of the square is = " + difference + "px";
+    fill('#C8A2CB');
+    stroke('#C8A2CB');
+    square(noseX, noseY, difference);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
+
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    console.log("leftwristX = " + leftWristX + "rightwristX = " + rightWristX + "difference = " + "px");
+
+    
+    }
+}
